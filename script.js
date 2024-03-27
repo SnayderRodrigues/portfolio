41df4a8b318a3b56dd9f7a5634167e3b7fad2e4a
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-03-26 23:41:41 -0300

    add smooth behavior for anchor links

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ class App {
 
   _initialize() {
     this._createLenis();
+    document.querySelectorAll('a[href^="#"]').forEach((el) => {
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+        const id = el.getAttribute("href")?.slice(1);
+        if (!id) return;
+        const target = document.getElementById(id);
+        if (target) {
+          target.scrollIntoView({ behavior: "smooth" });
+        }
+      });
+    });
   }
 
   _createLenis() {
